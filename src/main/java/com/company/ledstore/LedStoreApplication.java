cccbe6e7bf6808fef60d0f1ef0c9e72610e84936
Pavel Pershukevich <paulmojo88@gmail.com>
2024-01-07 21:16:18 +0300

    First commit

---
+++ src/main/java/com/company/ledstore/LedStoreApplication.java
@@ -0,0 +1,13 @@
+package com.company.ledstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/ledstore/LedStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.ledstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
