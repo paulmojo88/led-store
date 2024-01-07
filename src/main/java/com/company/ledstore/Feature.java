0305286ea6215bbd0b7e64f3942bb0d5d1452158
Pavel Pershukevich <paulmojo88@gmail.com>
2024-01-07 22:20:27 +0300

    Add entities

---
+++ src/main/java/com/company/ledstore/Feature.java
@@ -0,0 +1,32 @@
+package com.company.ledstore;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Feature {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Flux flux;
+    @Enumerated(EnumType.STRING)
+    private Power power;
+    @Enumerated(EnumType.STRING)
+    private Temperature temperature;
+    @Enumerated(EnumType.STRING)
+    private Length length;
+    @Enumerated(EnumType.STRING)
+    private Profile profile;
+    public enum Flux {LM1000, LM2000, LM4000}
+    public enum Power {W10, W20, W40}
+    public enum Temperature {K3000, K4000, K5000}
+    public enum Length {MM500, MM1000, MM2000}
+    public enum Profile {WOOD, ALUMINIUM}
+}
